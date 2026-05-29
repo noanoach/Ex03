ee543a7cb9b83cea89c5f5d265105e4079d24f95
noa.noach <noa.noach@post.runi.ac.il>
2026-05-29 18:07:21 +0300

    added content to exceptions

--- Ex03.GarageLogic/Exceptions/ValueRangeException.cs
+++ Ex03.GarageLogic/Exceptions/ValueRangeException.cs
@@ -1,10 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Ex03.GarageLogic.Exceptions
 {
-    internal class ValueRangeException
+    public class ValueRangeException : Exception
     {
+        private readonly float m_MinValue;
+        private readonly float m_MaxValue;
+
+        public float MinValue
+        {
+            get
+            {
+                return m_MinValue;
+            }
+        }
+
+        public float MaxValue
+        {
+            get
+            {
+                return m_MaxValue;
+            }
+        }
+
+        public ValueRangeException(float i_MinValue, float i_MaxValue)
+        {
+        }
+
+        public ValueRangeException(
+            string i_Message,
+            float i_MinValue,
+            float i_MaxValue)
+            : base(i_Message)
+        {
+        }
     }
 }
