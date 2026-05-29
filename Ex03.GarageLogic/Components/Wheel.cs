7ca2e6e1c51e2a0b90454e530f891f862ef26de6
noa.noach <noa.noach@post.runi.ac.il>
2026-05-29 11:43:37 +0300

    added new empty folders and files

---
+++ Ex03.GarageLogic/Components/Wheel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic.Components
+{
+    internal class Wheel
+    {
+    }
+}
---
+++ Ex03.GarageLogic/EnergySources/ElectricEnergySource.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic.EnergySources
+{
+    internal class ElectricEnergySource
+    {
+    }
+}
